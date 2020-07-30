af4c746571f89fab0af261f0c4eb8dd1ff56ce5f
Heli Hyvättinen <heli.hyvattinen@gmail.com>
2020-07-30 12:30:49 +0300

    Kovakoodatut liiikkumispisteet maastokoodeille

--- heksareitinhaku/src/main/java/heksareitinhaku/logic/WesnothMapInterpreter.java
+++ heksareitinhaku/src/main/java/heksareitinhaku/logic/WesnothMapInterpreter.java
@@ -17,7 +17,7 @@
 package heksareitinhaku.logic;
 
 /**
- * huolehtii Battle of Wesnoth -kartan tulkinnasta liikkumispisteiksi heksojen
+ * huolehtii Battle for Wesnoth -kartan tulkinnasta liikkumispisteiksi heksojen
  * välillä. Ei noudata pelin sääntöjä, vaan tulkitsee kartaa vapaasti.
  *
  * @author Heli Hyvättinen
@@ -30,14 +30,42 @@ public class WesnothMapInterpreter implements MapInterpreter {
     /**
      *
      * @param map
-     * @param width kartan leveys heksoina
+     *
      *
      */
     public WesnothMapInterpreter(String[][] map) {
         this.map = map;
         this.movementPointsPerTerrain = new int[400];
-        // Tähän kovakoodattujen liikkumispisteiden lisääminen taulukkoon
-
+        // meaning of codes from: https://wiki.wesnoth.org/TerrainCodesWML
+        // actual movement points are not from the game
+        movementPointsPerTerrain[0] = 3;  //A, arctic
+        movementPointsPerTerrain[1] = 1; // B, bridge, reuires special treatment
+        movementPointsPerTerrain[2] = 1; //C, castle
+        movementPointsPerTerrain[3] = 3; //D, desert
+        movementPointsPerTerrain[4] = 2; //E, embellishment
+        movementPointsPerTerrain[5] = 4; //F, forest
+        movementPointsPerTerrain[6] = 2; //G, grass e.g. flat
+        movementPointsPerTerrain[7] = 4; //H, hills
+        movementPointsPerTerrain[8] = 2;//I, interior
+        movementPointsPerTerrain[9] = -1; //J, testing
+        movementPointsPerTerrain[10] = 2; //K, keep
+        movementPointsPerTerrain[1] = 2; //L not used, reserved for future use
+        movementPointsPerTerrain[12] = 6; //M, mountains
+        movementPointsPerTerrain[13] = 2; //N, not used,reserved fot future use
+        movementPointsPerTerrain[14] = 2; //O
+        movementPointsPerTerrain[15] = 2; //P
+        movementPointsPerTerrain[16] = -1; //Q, un-walkable
+        movementPointsPerTerrain[17] = 1; //R, roads or rails, requires special treatment
+        movementPointsPerTerrain[18] = 5; //S, swamp
+        movementPointsPerTerrain[19] = 2; //T, toadstool i.e. fungus
+        movementPointsPerTerrain[20] = 3; //U, undergraound
+        movementPointsPerTerrain[21] = 2; //V, village
+        movementPointsPerTerrain[22] = 4; //w, water, shallow water is passable
+        //   in the Battle for Wesnoth
+        movementPointsPerTerrain[23] = -1; //X, impassable
+        movementPointsPerTerrain[24] = 2; //Y, reserved for UMC
+        movementPointsPerTerrain[25] = 2; //Z, reserved for UMC
+        //Special sytem stuff has _ before first letter, needs spelcial treatment
     }
 
     /**
@@ -84,11 +112,26 @@ public class WesnothMapInterpreter implements MapInterpreter {
 
         String fromHex = map[fromY][fromX];
         String toHex = map[toY][toX];
+        int base = toHex.charAt(0);
+        int overlay = -1;
+        for (int i = 1; i < toHex.length(); i++) {
+            if (toHex.charAt(i) == '^') {
+                overlay = toHex.charAt(i + 1);
+            }
+        }
 
         // Tähän teiden ja siltojen käsittey!
-        int index = toHex.charAt(0);
-
-        return movementPointsPerTerrain[index];
+        int baseMovementPoints = movementPointsPerTerrain[base - 'A'];
+        if (overlay > 0) {
+            int overlayMovementPoints = movementPointsPerTerrain[overlay - 'A'];
+            if (overlayMovementPoints < 0) {
+                return -1;
+            }
+            if (overlayMovementPoints > baseMovementPoints) {
+                baseMovementPoints = overlayMovementPoints;
+            }
+        }
+        return baseMovementPoints;
     }
 
     @Override
