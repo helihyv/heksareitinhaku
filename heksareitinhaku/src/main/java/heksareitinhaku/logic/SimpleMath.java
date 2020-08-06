3f23dbd3233b0b539c130ce98e02871ad06b6f8c
Heli Hyvättinen <heli.hyvattinen@gmail.com>
2020-08-07 00:05:04 +0300

    Lisätty oma luokka yleiskäyttöisille matikkafunktioille
    Itseisarvo, maksimi ja minimi.

---
+++ heksareitinhaku/src/main/java/heksareitinhaku/logic/SimpleMath.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2020 Heli Hyvättinen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package heksareitinhaku.logic;
+
+/**
+ * Provides some simple mathematic funtions
+ *
+ * @author Heli Hyvättinen
+ */
+public class SimpleMath {
+
+    public static int abs(int x) {
+        if (x < 0) {
+            return -x;
+        }
+
+        return x;
+    }
+
+    public static int max(int x1, int x2) {
+
+        if (x1 > x2) {
+            return x1;
+        }
+
+        return x2;
+    }
+
+    public static int min(int x1, int x2) {
+        if (x1 < x2) {
+            return x1;
+        }
+
+        return x2;
+    }
+
+}
